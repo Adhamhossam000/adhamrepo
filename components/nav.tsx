cf54e862a43235ef8dafe3512699e6bc41414fe5
Adhamhossam000 <142353051+Adhamhossam000@users.noreply.github.com>
2024-08-01 22:43:18 +0300

    Update nav.tsx

--- components/nav.tsx
+++ components/nav.tsx
@@ -15,7 +15,7 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
-import { SquareUserRound, UserRound , CircleUserRound } from 'lucide-react'; // Import the square-user-round icon
+import { SquareUserRound, UserRound, CircleUserRound } from "lucide-react"; // Import the square-user-round icon
 
 
 const spaceMono = Space_Mono({
@@ -26,21 +26,21 @@ const spaceMono = Space_Mono({
 
 export function Nav() {
   return (
-    <div className="h-14 py-2 px-2 md:px-8 border-b flex items-center">
+    <div className="h-14 py-2 px-2 md:px-8 border-b flex items-center fade-in">
       <div className="flex flex-1 items-center">
         <Link href="/">
           <h1 className={cn("font-light text-xl", spaceMono.className)}>
-          <span className="text-white-700 ml-2 text-sm">
+            <span className="text-white-700 ml-2 text-sm ">
               Adham Image Generator ⚡️
             </span>
           </h1>
         </Link>
       </div>
       <div className="flex flex-none items-center space-x-4">
-      <Drawer>
+        <Drawer>
           <DrawerTrigger>
             <Button variant="ghost">
-              <CircleUserRound className="text-sm"/>
+              <CircleUserRound className="text-sm" />
             </Button>
           </DrawerTrigger>
           <DrawerContent>
@@ -50,21 +50,45 @@ export function Nav() {
                 Navigate to my social media profiles:
               </DrawerDescription>
             </DrawerHeader>
-            <div className="drawer-body" style={{ display: 'flex', flexWrap: 'wrap', gap: '16px', padding: '16px', justifyContent: 'center' }}>
-              <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+            <div
+              className="drawer-body"
+              style={{
+                display: "flex",
+                flexWrap: "wrap",
+                gap: "16px",
+                padding: "16px",
+                justifyContent: "center",
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "16px",
+                }}
+              >
                 <Link href="https://github.com/Adhamhossam000" passHref>
-                  <Button style={{ width: '200px' }}>GitHub</Button>
+                  <Button style={{ width: "200px" }}>GitHub</Button>
                 </Link>
                 <Link href="https://instagram.com/adhamhossam18" passHref>
-                  <Button style={{ width: '200px' }}>Instagram</Button>
+                  <Button style={{ width: "200px" }}>Instagram</Button>
                 </Link>
               </div>
-              <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
-                <Link href="https://www.linkedin.com/in/adham-hossam-72b3422b7/" passHref>
-                  <Button style={{ width: '200px' }}>LinkedIn</Button>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "16px",
+                }}
+              >
+                <Link
+                  href="https://www.linkedin.com/in/adham-hossam-72b3422b7/"
+                  passHref
+                >
+                  <Button style={{ width: "200px" }}>LinkedIn</Button>
                 </Link>
                 <Link href="https://x.com/AdhamHo62349992" passHref>
-                  <Button style={{ width: '200px' }}>X (Twitter)</Button>
+                  <Button style={{ width: "200px" }}>X (Twitter)</Button>
                 </Link>
               </div>
             </div>
@@ -75,7 +99,6 @@ export function Nav() {
             </DrawerFooter>
           </DrawerContent>
         </Drawer>
-        
       </div>
     </div>
   );
